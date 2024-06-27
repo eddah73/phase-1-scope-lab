780d8a79783cde776cee634ffd7aff2c8df0d890
eddah73 <eddahsjr@gmail.com>
2024-06-27 22:28:37 +0300

    scope in javascript

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+// upperCaseCustomerName();
+//ommiting const,var,let key word the variable become global
+function setBestCustomer(){
+     bestCustomer ='not bob';
+     return bestCustomer
+
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+const  leastFavoriteCustomer = "poor bob";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "rich bob";
+    return leastFavoriteCustomer;
+
+}
